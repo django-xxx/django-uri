e6695a58c5c860a14139a33d9fbc77c0332cf317
Brightcells <brightcells@gmail.com>
2019-10-22 17:07:39 +0800

    :art: Return url,changed

--- uri/django_uri.py
+++ uri/django_uri.py
@@ -4,7 +4,7 @@
 def repalce_uri_parts(request, new, olds=[]):
     if not isinstance(olds, list):
         olds = [olds]
-    url = request.build_absolute_uri()
+    new_url = old_url = request.build_absolute_uri()
     for old in olds:
-        url = url.replace(old, new)
-    return url
+        new_url = new_url.replace(old, new)
+    return new_url, new_url != old_url
